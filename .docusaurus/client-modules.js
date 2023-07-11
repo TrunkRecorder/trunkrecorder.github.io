e14d620c84c697a3d618c6906c0f4cbe43bac2b8
Luke Berndt <lukekb@gmail.com>
2023-07-10 22:42:51 -0400

    Basic Site

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/luke/Projects/TrunkRecorder/site/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/luke/Projects/TrunkRecorder/site/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/luke/Projects/TrunkRecorder/site/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/luke/Projects/TrunkRecorder/site/src/css/custom.css'),
+];
