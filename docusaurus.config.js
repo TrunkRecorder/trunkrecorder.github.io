3de2510fe2757302ae6d68e6deef32d0626fc918
Luke Berndt <lukekb@gmail.com>
2023-07-11 21:29:28 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://TrunkRecorder.github.io/site/',
+  url: 'https://TrunkRecorder.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -19,6 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'TrunkRecorder', // Usually your GitHub org/user name.
+  trailingSlash: false,
   projectName: 'site', // Usually your repo name.
 
   onBrokenLinks: 'throw',
